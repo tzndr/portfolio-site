04a25c07f652071e8d06c1556442f04bf37b50e4
Timothy Zander <connect@timzander.com>
2018-10-25 16:41:21 -0500

    Add 'Featured Work' page

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,39 @@
-var typeIt = function() {
-  new TypeIt('.type-it', {
-    strings: ['Hello.', "I'm Tim Zander,"],
-    cursor: false
-  });
+var catalogAppDescription = "A user-friendly, editable catalog application built with HTML, CSS, and Python " +
+  "with Flask and SQLAlchemy. This application queries a SQL database and uses CRUD " +
+  "(Create, Read, Update, Delete) functionality to allow a logged-in user to add, " +
+  "remove, and edit the catalog's categories and products."
+
+var $catalogDescription = $('#catalog-description');
+
+var ViewModel = {
+  init: function() {
+    $catalogDescription.text(catalogAppDescription);
+  }
 }
 
-var retypeIt = function() {
-  new TypeIt('.retype', {
-    strings: ["a coder", "a life-long learner", "a creator", "a leader",
-      "a Full-Stack Developer."],
-    breakLines: false,
-    autoStart: false,
-    deleteSpeed: 40,
-    loop: false
-  });
-}
+ViewModel.init();
 
-typeIt();
-setTimeout(retypeIt, 4000);
+
+
+
+
+//var typeIt = function() {
+//  new TypeIt('.type-it', {
+//    strings: ['Hello.', "I'm Tim Zander,"],
+//    cursor: false
+//  });
+//}
+
+//var retypeIt = function() {
+//  new TypeIt('.retype', {
+//    strings: ["a coder", "a life-long learner", "a creator", "a leader",
+//      "a Full-Stack Developer."],
+//    breakLines: false,
+//    autoStart: false,
+//    deleteSpeed: 40,
+//    loop: false
+//  });
+//}
+
+//typeIt();
+//setTimeout(retypeIt, 4000);
